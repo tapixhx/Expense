0e76ba37f68f26671e2ac291f018d3d7894277cb
tapixhx <tapish818@gmail.com>
2020-12-04 22:20:40 +0530

    Transactions added

---
+++ Expense/lib/widgets/transaction_list.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+
+  TransactionList(this.transactions);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height:300,
+          child: ListView.builder(
+            itemBuilder: (ctx, index) {
+              return Card(
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(border: Border.all(
+                    color: Colors.purple, 
+                    width:2,
+                  ),),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$ ${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text(
+                    transactions[index].title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(transactions[index].date),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                      color: Colors.grey,
+                    ),),
+                ],)
+              ],),
+            );
+            },
+            itemCount: transactions.length,
+    ),);
+      
+  }
+}
